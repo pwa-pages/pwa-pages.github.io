0865a760b5a48af21bd728f40d3eca9f9b245e1c
pebblerye <bertvandenbosch76@gmail.com>
2024-03-30 22:18:27 +0100

    Automated commit za 30 mrt 2024 22:18:27 CET

--- rosen-watcher-pwa/push.sh
+++ rosen-watcher-pwa/push.sh
@@ -1,3 +1,5 @@
+sudo rm -rf node_modules
+sudo rm -rf .angular
 git add .
 git commit -m "Automated commit $(date)"
 git push origin HEAD --force
--- rosen-watcher-pwa/src/app/app.module.ts
+++ rosen-watcher-pwa/src/app/app.module.ts
@@ -11,7 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule, // Add HttpClientModule to imports
+    HttpClientModule, 
     CommonModule
   ],
   providers: [],
