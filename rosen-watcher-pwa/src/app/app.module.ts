714ffd45ab81993c6280c2668b3c867f141cce3f
pebblerye <bertvandenbosch76@gmail.com>
2024-04-01 11:03:57 +0200

    Automated commit ma  1 apr 2024 11:03:57 CEST

--- rosen-watcher-pwa/src/app/app.module.ts
+++ rosen-watcher-pwa/src/app/app.module.ts
@@ -1,9 +1,10 @@
 // app.module.ts
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'; // Import HttpClientModule
 import { CommonModule } from '@angular/common'; // Import CommonModule
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,12 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     HttpClientModule, 
-    CommonModule
+    CommonModule, ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: !isDevMode(),
+  // Register the ServiceWorker as soon as the application is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
+})
   ],
   providers: [],
   bootstrap: [AppComponent]
