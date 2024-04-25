0ffeb15b048106548e84cdd4b174857911debd25
pebblerye <bertvandenbosch76@gmail.com>
2024-04-25 22:44:39 +0200

    Automated commit do 25 apr 2024 22:44:39 CEST

--- rosen-watcher-pwa/src/app/statistics/performance.ts
+++ rosen-watcher-pwa/src/app/statistics/performance.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges, ViewEncapsulation } from '@angular/core';
 import { EventService, EventType } from '../service/event.service';
 import { StorageService } from '../service/storage.service';
 import { DataService } from '../service/data.service';
@@ -6,6 +6,7 @@ import { FeatureService } from '../service/featureservice';
 import { BaseWatcherComponent } from '../basewatchercomponent';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'performance',
@@ -20,6 +21,7 @@ export class Performance extends BaseWatcherComponent implements OnInit {
   addresses: string[];
   noAddresses: boolean = false;
   showPermitsLink: boolean = false;
+  chart: Chart<"bar", any[][], unknown> | undefined;
 
   constructor(private location: Location, private route: ActivatedRoute, private storageService: StorageService, private dataService: DataService, private featureService: FeatureService, eventService: EventService) {
 
@@ -29,8 +31,8 @@ export class Performance extends BaseWatcherComponent implements OnInit {
     this.performanceChart = [];
   }
 
-  async retrieveData(): Promise<void>  {
-    
+  async retrieveData(): Promise<void> {
+
     this.performanceChart = await this.dataService.getPerformanceChart();
 
   }
@@ -47,12 +49,152 @@ export class Performance extends BaseWatcherComponent implements OnInit {
 
     this.route.params.subscribe(async params => {
       await this.retrieveData();
-     
+      this.updateChart();
     });
 
 
   }
 
-  title = 'rosen-watcher-pwa';
+  reduceChartData(data: any[], targetPoints: number): any[] {
+    let remainingPoints = data.length - targetPoints;
+    if (remainingPoints <= 0) {
+      return data;
+    }
 
+    let points = data.slice();
+
+    while (remainingPoints > 0) {
+      let minArea = Infinity;
+      let indexToRemove = -1;
+
+
+      for (let i = 1; i < points.length - 1; i++) {
+        let area = this.calculateTriangleArea(points[i - 1], points[i], points[i + 1]);
+        if (area < minArea) {
+          minArea = area;
+          indexToRemove = i;
+        }
+      }
+
+      if (indexToRemove !== -1) {
+        points.splice(indexToRemove, 1);
+        remainingPoints--;
+      } else {
+        break;
+      }
+    }
+
+    return points;
+  }
+
+  calculateTriangleArea(p1: { x: Date, y: number }, p2: { x: Date, y: number }, p3: { x: Date, y: number }): number {
+    return Math.abs((p1.x.getTime() * (p2.y - p3.y) + p2.x.getTime() * (p3.y - p1.y) + p3.x.getTime() * (p1.y - p2.y)) / 2);
+  }
+
+  private createDataSet(i: number): any {
+
+    const chartColors: string[] = [
+      '#1f77b4', // Blue
+      '#ff7f0e', // Orange
+      '#2ca02c', // Green
+      '#d62728', // Red
+      '#9467bd', // Purple
+      '#8c564b', // Brown
+      '#e377c2', // Pink
+      '#7f7f7f', // Gray
+      '#17becf',  // Turquoise
+      '#bcbd22' // Yellow-Green
+    ];
+
+    var chartColor = chartColors[(i) % 10];
+    return {
+      label: "",
+      data: [
+      ],
+      borderColor: chartColor,
+      backgroundColor: chartColor,
+      pointBackgroundColor: chartColor
+    };
+  }
+
+  updateChart(): void {
+    if (!this.chart) {
+      var dataSets = [];
+      var cnt = this.performanceChart.length;
+      for (var i = 0; i < cnt; i++) {
+        dataSets.push(
+          this.createDataSet(i)
+        );
+      }
+
+      for (var i = 0; i < this.performanceChart.length; i++) {
+        dataSets[i].data = this.performanceChart[i].chart;
+        dataSets[i].label = 'Address: ' + this.performanceChart[i].addressForDisplay;
+      }
+
+      this.createChart(dataSets);
+    }
+
+
+  }
+
+
+  createChart(datasets: any[]): Chart<"bar", any[][], unknown> {
+    return new Chart("PerformanceChart", {
+      type: 'bar',
+      data: {
+        datasets: datasets
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          y: {
+            beginAtZero: true,
+            stacked: true,
+
+            grid: {
+              color: 'rgba(0, 0, 0, 0.1)',
+            },
+            ticks: {
+              callback: function (value) {
+                return value as number / 1000;
+              }
+            }
+          },
+          x: {
+            type: 'time',
+            stacked: true,
+            time: {
+              unit: 'week',
+            },
+            grid: {
+              color: 'rgba(0, 0, 0, 0.1)',
+            }
+          }
+        },
+        plugins: {
+          tooltip: {
+            backgroundColor: 'rgba(0, 0, 0, 0.7)',
+            bodyFont: {
+              size: 14,
+            },
+            titleFont: {
+              size: 16,
+              weight: 'bold',
+            }
+          },
+          legend: {
+            labels: {
+              font: {
+                size: 14,
+              }
+            }
+          }
+          ,
+        }
+      }
+    });
+  }
+  title = 'rosen-watcher-pwa';
 }
