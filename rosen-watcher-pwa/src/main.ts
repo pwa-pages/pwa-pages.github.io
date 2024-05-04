6a3d19337ccb8f2c1fe9ffc030b791c96123d8ed
pebblerye <bertvandenbosch76@gmail.com>
2024-05-04 23:57:08 +0200

    Automated commit za  4 mei 2024 23:57:08 CEST

--- rosen-watcher-pwa/src/main.ts
+++ rosen-watcher-pwa/src/main.ts
@@ -1,9 +1,9 @@
 import '@angular/compiler';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module'; // Import AppModule
-
-
+import './swipe';
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
