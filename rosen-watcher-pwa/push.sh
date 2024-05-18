3702a0d7e09116d9823915b5987cbf1b3b85bc89
pebblerye <bertvandenbosch76@gmail.com>
2024-05-18 14:23:30 +0200

    Automated commit za 18 mei 2024 14:23:30 CEST

--- rosen-watcher-pwa/push.sh
+++ rosen-watcher-pwa/push.sh
@@ -16,8 +16,7 @@ echo "Script execution confirmed."
 
 npm install
 ng build --configuration=production --base-href ./ --deploy-url ./
-sudo rm -rf node_modules
-sudo rm -rf .angular
+./clean.sh
 cp -R dist/rosen-watcher-pwa/browser/* .
 git add .
 git commit -m "Automated commit $(date)"
