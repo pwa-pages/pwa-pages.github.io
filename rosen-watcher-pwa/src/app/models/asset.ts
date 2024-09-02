9a635c15c264a5c021300eb59c762cd5d8a68136
pebblerye <bertvandenbosch76@gmail.com>
2024-09-02 15:48:17 +0200

    Automated commit ma  2 sep 2024 15:48:17 CEST

--- rosen-watcher-pwa/src/app/models/asset.ts
+++ rosen-watcher-pwa/src/app/models/asset.ts
@@ -1,17 +1,24 @@
 export class Asset {
-    tokenId: string;
-    index: number;
-    amount: number;
-    name: string;
-    decimals: number;
-    type: string;
+  tokenId: string;
+  index: number;
+  amount: number;
+  name: string;
+  decimals: number;
+  type: string;
 
-    constructor(tokenId: string, index: number, amount: number, name: string, decimals: number, type: string) {
-        this.tokenId = tokenId;
-        this.index = index;
-        this.amount = amount;
-        this.name = name;
-        this.decimals = decimals;
-        this.type = type;
-    }
+  constructor(
+    tokenId: string,
+    index: number,
+    amount: number,
+    name: string,
+    decimals: number,
+    type: string,
+  ) {
+    this.tokenId = tokenId;
+    this.index = index;
+    this.amount = amount;
+    this.name = name;
+    this.decimals = decimals;
+    this.type = type;
+  }
 }
