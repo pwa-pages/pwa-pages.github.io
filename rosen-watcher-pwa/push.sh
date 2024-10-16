0162cb64c739b9e3263d60d637ba0cad8ddc3c0b
pebblerye <bertvandenbosch76@gmail.com>
2024-10-16 12:31:36 +0200

    Automated commit wo 16 okt 2024 12:31:36 CEST

--- rosen-watcher-pwa/push.sh
+++ rosen-watcher-pwa/push.sh
@@ -19,6 +19,9 @@ ng build --configuration=production --base-href ./ --deploy-url ./
 ./clean.sh
 cp -R dist/rosen-watcher-pwa/browser/* .
 git add .
+cp index.html ../404.html
+git add ../404.html
+
 git commit -m "Automated commit $(date)"
 git push origin HEAD --force
 
