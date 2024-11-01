9fe1dc23ac3ca9cd7a5244ae5d387a338024338f
pebblerye <bertvandenbosch76@gmail.com>
2024-11-01 21:36:29 +0100

    Automated commit vr  1 nov 2024 21:36:29 CET

--- rosen-watcher-pwa/src/app/statistics/watchers.component.ts
+++ rosen-watcher-pwa/src/app/statistics/watchers.component.ts
@@ -3,18 +3,18 @@ import { EventService } from '../service/event.service';
 import { WatchersDataService } from '../service/watchers.data.service';
 import { SwipeService } from '../service/swipe.service';
 import { BaseWatcherComponent } from '../basewatchercomponent';
-import { Observable, EMPTY, combineLatest, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { ChainType } from '../../service/ts/models/chaintype';
 import { AsyncPipe } from '@angular/common';
 import { Token } from '../../service/ts/models/token';
 import { CommonModule } from '@angular/common'; // Import CommonModule
 
-function createChainObservable<T>(): { [key in ChainType]: Observable<T | undefined> } {
-  return Object.fromEntries(
-    Object.values(ChainType).map((key) => [key, of(undefined)])
-  ) as { [key in ChainType]: Observable<T | undefined> };
+function createChainNumber(): { [key in ChainType]: number | undefined } {
+  return Object.fromEntries(Object.values(ChainType).map((key) => [key, undefined])) as {
+    [key in ChainType]: number | undefined;
+  };
 }
+
 @Component({
   selector: 'app-watchers',
   templateUrl: './watchers.html',
@@ -22,17 +22,15 @@ function createChainObservable<T>(): { [key in ChainType]: Observable<T | undefi
   imports: [AsyncPipe, CommonModule],
 })
 export class WatchersComponent extends BaseWatcherComponent implements OnInit {
-  watcherCount: Observable<number>;
+  chainWatcherCount = createChainNumber();
+  chainPermitCount = createChainNumber();
+  chainLockedRSN = createChainNumber();
+  chainLockedERG = createChainNumber();
 
-  chainWatcherCount = createChainObservable<number>();
-  chainPermitCount = createChainObservable<number>();
-  chainLockedRSN = createChainObservable<number>();
-  chainLockedERG = createChainObservable<number>();
-
-  totalWatcherCount: Observable<number | undefined>;
-  totalPermitCount: Observable<number | undefined>;
-  totalLockedRSN: Observable<number | undefined>;
-  totalLockedERG: Observable<number | undefined>;
+  totalWatcherCount: number | undefined;
+  totalPermitCount: number | undefined;
+  totalLockedRSN: number | undefined;
+  totalLockedERG: number | undefined;
 
   constructor(
     private watchersDataService: WatchersDataService,
@@ -40,52 +38,36 @@ export class WatchersComponent extends BaseWatcherComponent implements OnInit {
     swipeService: SwipeService,
   ) {
     super(eventService, swipeService);
-    this.watcherCount = EMPTY;
-    this.totalWatcherCount = EMPTY;
-    this.totalPermitCount = EMPTY;
-    this.totalLockedRSN = EMPTY;
-    this.totalLockedERG = EMPTY;
+
+  }
+
+  setLockedAmounts(chainType: ChainType): void {
+    this.chainLockedRSN[chainType] = (this.chainPermitCount[chainType] ?? 0) * 3000 + (this.chainWatcherCount[chainType] ?? 0) * 30000;
+    this.chainLockedERG[chainType] = (this.chainWatcherCount[chainType] ?? 0) * 800;
+    this.totalWatcherCount = Object.values(this.chainWatcherCount).reduce((accumulator: number | undefined, currentValue: number | undefined) => (accumulator ?? 0) + (currentValue ?? 0), 0)
+    this.totalPermitCount = Object.values(this.chainPermitCount).reduce((accumulator: number | undefined, currentValue: number | undefined) => (accumulator ?? 0) + (currentValue ?? 0), 0)
+    this.totalLockedRSN = Object.values(this.chainLockedRSN).reduce((accumulator: number | undefined, currentValue: number | undefined) => (accumulator ?? 0) + (currentValue ?? 0), 0)
+    this.totalLockedERG = Object.values(this.chainLockedERG).reduce((accumulator: number | undefined, currentValue: number | undefined) => (accumulator ?? 0) + (currentValue ?? 0), 0)
   }
 
   override async ngOnInit(): Promise<void> {
     super.ngOnInit();
-
     this.initSwipe('/statistics', '/performance');
-
     const watcherInfo$ = this.watchersDataService.getWatchersInfo();
 
-    // Assign observables
-    this.watcherCount = watcherInfo$.pipe(
-      map((watcherInfo) =>
-        watcherInfo.tokens
-          .filter((token: Token) => token.name !== 'RSN')
-          .reduce((sum: number, token: Token) => sum + token.amount, 0),
-      ),
-    );
-
-    Object.values(ChainType).forEach(c => {
-      this.chainWatcherCount[c] = watcherInfo$.pipe(
+    Object.values(ChainType).forEach((c) => {
+      watcherInfo$.pipe(
         map(
           (watcherInfo) =>
             watcherInfo.tokens.find((token: Token) => token.name === 'rspv2' + c + 'AWC')?.amount,
         ),
-      );
+      ).subscribe((amount) => { this.chainWatcherCount[c] = amount; this.setLockedAmounts(c); });
 
-      this.chainPermitCount[c] = this.watchersDataService.getPermitsInfo(c).pipe(map((permitsInfo) => permitsInfo?.amount));
-
-      this.chainLockedRSN[c] = combineLatest([this.chainPermitCount[c], this.chainWatcherCount[c]]).pipe(
-        map(([p, w]) => (p ?? 0) * 3000 + (w ?? 0) * 30000),
-      );
-
-      this.chainLockedERG[c] = this.chainWatcherCount[c].pipe(
-        map(w => (w ?? 0) * 800),
-      );
-
+      this.watchersDataService
+        .getPermitsInfo(c)
+        .pipe(map((permitsInfo) => permitsInfo?.amount))
+        .subscribe((amount) => { this.chainPermitCount[c] = amount; this.setLockedAmounts(c); });
     });
 
-    this.totalWatcherCount = combineLatest(Object.values(this.chainWatcherCount)).pipe(map(([eth, btc, ada, erg]) => (eth ?? 0) + (btc ?? 0) + (ada ?? 0) + (erg ?? 0)));
-    this.totalPermitCount = combineLatest(Object.values(this.chainPermitCount)).pipe(map(([eth, btc, ada, erg]) => (eth ?? 0) + (btc ?? 0) + (ada ?? 0) + (erg ?? 0)));
-    this.totalLockedRSN = combineLatest(Object.values(this.chainLockedRSN)).pipe(map(([eth, btc, ada, erg]) => (eth ?? 0) + (btc ?? 0) + (ada ?? 0) + (erg ?? 0)));
-    this.totalLockedERG = combineLatest(Object.values(this.chainLockedERG)).pipe(map(([eth, btc, ada, erg]) => (eth ?? 0) + (btc ?? 0) + (ada ?? 0) + (erg ?? 0)));
   }
 }
