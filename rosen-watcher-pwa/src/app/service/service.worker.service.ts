72a3a66937f34443976b7d2536701978fcc67b05
pebblerye <bertvandenbosch76@gmail.com>
2024-12-10 11:03:26 +0100

    Automated commit di 10 dec 2024 11:03:26 CET

--- rosen-watcher-pwa/src/app/service/service.worker.service.ts
+++ rosen-watcher-pwa/src/app/service/service.worker.service.ts
@@ -19,7 +19,7 @@ export function initializeServiceWorkerService(serviceWorkerService: ServiceWork
   providedIn: 'root',
 })
 export class ServiceWorkerService {
-  private  currentProfile:string | null | undefined = null;
+  //private currentProfile: string | null | undefined = null;
 
   constructor(private eventService: EventService) {
     this.listenForServiceWorkerMessages();
@@ -46,7 +46,7 @@ export class ServiceWorkerService {
 
   sendMessageToServiceWorker(message: ServiceWorkerMessage) {
     if (navigator.serviceWorker && navigator.serviceWorker.controller) {
-      this.currentProfile = message.data as string | null | undefined ;
+      //this.currentProfile = message.data as string | null | undefined;
       navigator.serviceWorker.controller.postMessage(message);
     } else {
       console.error('No active service worker found to send message');
@@ -58,7 +58,12 @@ export class ServiceWorkerService {
       navigator.serviceWorker.addEventListener('message', (event) => {
         const message = event.data as ServiceWorkerMessage;
 
-        console.log('Received message from service worker of type ' + message.type + ", profile " + message.profile);
+        console.log(
+          'Received message from service worker of type ' +
+            message.type +
+            ', profile ' +
+            message.profile,
+        );
 
         this.handleServiceWorkerMessage(message);
 
@@ -76,19 +81,18 @@ export class ServiceWorkerService {
 
   handleServiceWorkerMessage(message: ServiceWorkerMessage) {
     console.log('Handling message from service worker:', message);
-    
-    
-    if(message.profile !== this.currentProfile){
-      return;
-    }
 
+    /*
+    if (message.profile !== this.currentProfile) {
+      return;
+    }*/
 
     if ((Object.values(EventType) as string[]).includes(message.type)) {
       if (message.data) {
         this.eventService.sendEventWithData(
           message.type as EventType,
           message.data as EventData,
-          message.profile
+          message.profile,
         );
       } else {
         this.eventService.sendEvent(message.type as EventType);
