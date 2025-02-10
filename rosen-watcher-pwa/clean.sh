66462b276245ba13a51fb7949035657714ca4d82
pebblerye <bertvandenbosch76@gmail.com>
2025-02-10 19:02:30 +0100

    Automated commit ma 10 feb 2025 19:02:30 CET

--- rosen-watcher-pwa/clean.sh
+++ rosen-watcher-pwa/clean.sh
@@ -1,7 +1,7 @@
-sudo rm -rf main-*.js
-sudo rm -rf poly-*.js
-sudo rm -rf *worker*.js
-sudo rm -rf styles-*.css
+sudo find . -type f -name "main-*.js" -mtime +10 -exec rm -f {} \;
+sudo find . -type f -name "poly-*.js" -mtime +10 -exec rm -f {} \;
+sudo find . -type f -name "*worker*.js" -mtime +10 -exec rm -f {} \;
+sudo find . -type f -name "styles-*.css" -mtime +10 -exec rm -f {} \;
 rm -rf shared
 sudo rm -rf node_modules
 sudo rm -rf .angular
