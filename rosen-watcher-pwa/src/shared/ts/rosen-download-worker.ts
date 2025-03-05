a1b4d306a933b7e37d4500aaaacda20e787cabe3
pebblerye <bertvandenbosch76@gmail.com>
2025-03-05 21:12:05 +0100

    Automated commit wo  5 mrt 2025 21:12:05 CET

--- rosen-watcher-pwa/src/shared/ts/rosen-download-worker.ts
+++ rosen-watcher-pwa/src/shared/ts/rosen-download-worker.ts
@@ -73,7 +73,7 @@ self.addEventListener('message', async (event: MessageEvent) => {
           profile: profile,
         });
 
-        console.log('Downloading perftxs.');
+        console.log('Downloading perftxsss.');
         const perfTxs = await chainPerformanceDataService.getPerfTxs();
         sendMessageToClients({
           type: 'PerfTxsChanged',
