6f34e16b49f3527c79d4a7d528d40617ca5aded5
pebblerye <bertvandenbosch76@gmail.com>
2025-03-09 18:55:14 +0100

    Automated commit zo  9 mrt 2025 18:55:14 CET

--- rosen-watcher-pwa/prep_web_component.sh
+++ rosen-watcher-pwa/prep_web_component.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cp dist/rosen-watchers/assets/*.html  dist/rosen-watchers/
+cp dist/rosen_chain-performance/assets/*.html  dist/rosen_chain-performance/
 
 
 dir="dist/rosen-watchers/" 
@@ -14,19 +15,38 @@ for file in "$dir"main*.js "$dir"runtime*.js "$dir"styles*.css; do
     # Replace {{main.js}} or {{runtime.js}} or {{styles.css}} with the filename in web_component.html
     if [[ "$filename" == main*.js ]]; then
       sed -i "s|{{main.js}}|$filename|g" dist/rosen-watchers/rosen_watchers.html
-      sed -i "s|{{main.js}}|$filename|g" dist/rosen-watchers/rosen_chain-performance.html
-      echo "Replaced {{main.js}} with $filename in dist/rosen-watchers/rosen_watchers.html"
+            echo "Replaced {{main.js}} with $filename in dist/rosen-watchers/rosen_watchers.html"
     elif [[ "$filename" == runtime*.js ]]; then
       sed -i "s|{{runtime.js}}|$filename|g" dist/rosen-watchers/rosen_watchers.html
-      sed -i "s|{{runtime.js}}|$filename|g" dist/rosen-watchers/rosen_chain-performance.html
-      echo "Replaced {{runtime.js}} with $filename in dist/rosen-watchers/rosen_watchers.html"
+            echo "Replaced {{runtime.js}} with $filename in dist/rosen-watchers/rosen_watchers.html"
     elif [[ "$filename" == styles*.css ]]; then
       sed -i "s|{{styles.css}}|$filename|g" dist/rosen-watchers/rosen_watchers.html
-      sed -i "s|{{styles.css}}|$filename|g" dist/rosen-watchers/rosen_chain-performance.html
       echo "Replaced {{styles.css}} with $filename in dist/rosen-watchers/rosen_watchers.html"
+    fi
+  fi
+done
+
+dir="dist/rosen_chain-performance/" 
+
+for file in "$dir"main*.js "$dir"runtime*.js "$dir"styles*.css; do
+  if [[ -f "$file" ]]; then
+    # Extract the filename without the path
+    filename=$(basename "$file")
+
+    # Replace {{main.js}} or {{runtime.js}} or {{styles.css}} with the filename in web_component.html
+    if [[ "$filename" == main*.js ]]; then
+      sed -i "s|{{main.js}}|$filename|g" dist/rosen-watchers/rosen_chain-performance.html
+      echo "Replaced {{main.js}} with $filename in dist/rosen-watchers/rosen_chain-performance.html"
+    elif [[ "$filename" == runtime*.js ]]; then
+      sed -i "s|{{runtime.js}}|$filename|g" dist/rosen-watchers/rosen_chain-performance.html
+      echo "Replaced {{runtime.js}} with $filename in dist/rosen-watchers/rosen_chain-performance.html"
+    elif [[ "$filename" == styles*.css ]]; then
+      sed -i "s|{{styles.css}}|$filename|g" dist/rosen-watchers/rosen_chain-performance.html
+      echo "Replaced {{styles.css}} with $filename in dist/rosen-watchers/rosen_chain-performance.html"
     fi
   fi
 done
 
 rm web_component/*
 cp dist/rosen-watchers/* web_component/
+cp dist/rosen_chain-performance/* web_component/
