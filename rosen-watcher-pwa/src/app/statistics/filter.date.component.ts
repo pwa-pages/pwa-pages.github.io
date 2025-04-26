6a730a5a298775445f664a0438525f1634d2c2ef
pebblerye <bertvandenbosch76@gmail.com>
2025-04-26 09:18:46 +0200

    Automated commit za 26 apr 2025  9:18:46 CEST

--- rosen-watcher-pwa/src/app/statistics/filter.date.component.ts
+++ rosen-watcher-pwa/src/app/statistics/filter.date.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, HostListener, Input, Output } from '@angular/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatInputModule } from '@angular/material/input';
 import { MatNativeDateModule } from '@angular/material/core';
@@ -11,10 +11,24 @@ import { FormsModule } from '@angular/forms';
   imports: [MatDatepickerModule, MatInputModule, MatNativeDateModule, FormsModule],
 })
 export class FilterDateComponent {
+  constructor(private eRef: ElementRef) {}
+
   @Input() fromDate: Date | null = null;
   @Input() toDate: Date | null = null;
   @Output() dateRangeChanged = new EventEmitter<{ from: Date | null; to: Date | null }>();
   @Output() dateRangeCanceled = new EventEmitter();
+
+  @HostListener('document:click', ['$event'])
+  handleClickOutside(event: MouseEvent) {
+    if (
+      event.target instanceof HTMLElement &&
+      !this.eRef.nativeElement.contains(event.target) &&
+      !event.target.closest('.datepicker-panel') && // <- NEW line to allow clicks inside datepicker
+      !event.target.classList.contains('rsn_filter')
+    ) {
+      this.onCancel();
+    }
+  }
 
   onOk() {
     this.dateRangeChanged.emit({
