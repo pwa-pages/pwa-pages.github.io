6230a5cfba004a6e2fa9365eff92a597e9d4acfa
pebblerye <bertvandenbosch76@gmail.com>
2025-06-16 18:14:26 +0200

    Automated commit ma 16 jun 2025 18:14:26 CEST

--- paintings/src/app/models/painting.ts
+++ paintings/src/app/models/painting.ts
@@ -5,4 +5,5 @@ export interface Painting {
   year: number;
   dimensions: string;
   status: string;
+  type: string;
 }
--- paintings/src/app/service/paintings.service.ts
+++ paintings/src/app/service/paintings.service.ts
@@ -7,12 +7,13 @@ import { Painting } from "../models/painting";
 export class PaintingService {
   private paintings: Painting[] = [
     {
-      id: "50x64_frame1",
+      id: "64x50_frame1",
       title: "Andy Warhol is not dead",
       artist: "Alexander van den Bosch",
       year: 1988,
       dimensions: "50x64",
       status: "framed",
+      type: "oilpainting",
     },
     {
       id: "75x54_frame2",
@@ -21,22 +22,34 @@ export class PaintingService {
       year: 1985,
       dimensions: "75x54",
       status: "framed",
+      type: "drawing",
     },
     {
-      id: "100x120_rol1",
+      id: "120x100_rol1",
       title: "No Title",
       artist: "Alexander van den Bosch",
       year: 0,
       dimensions: "100x120",
       status: "rolled",
+      type: "oilpainting",
     },
     {
-      id: "100x138_rol2",
+      id: "138x100_rol2",
       title: "No Title",
       artist: "Alexander van den Bosch",
       year: 1979,
       dimensions: "100x138",
       status: "rolled",
+      type: "oilpainting",
+    },
+    {
+      id: "64x50_frame3",
+      title: "Inferno 4",
+      artist: "Alexander van den Bosch",
+      year: 1987,
+      dimensions: "100x138",
+      status: "rolled",
+      type: "drawing",
     },
   ];
 
