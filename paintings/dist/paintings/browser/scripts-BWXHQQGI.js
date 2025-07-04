fed81a9d6b78e0e30e5cfef12419ce1dc2cdabfc
pebblerye <bertvandenbosch76@gmail.com>
2025-07-04 21:06:43 +0200

    Automated commit vr  4 jul 2025 21:06:43 CEST

---
+++ paintings/dist/paintings/browser/scripts-BWXHQQGI.js
@@ -0,0 +1,1 @@
+typeof importScripts=="function"&&(self.addEventListener("install",e=>{console.log("[Service Worker] Installing new version...calling skipWaiting()"),self.skipWaiting(),console.log("[Service Worker] Installing new version...done skipWaiting()")}),self.addEventListener("activate",e=>{console.log("[Service Worker] Activated new version!")}));
