b3aff16cf1ae1afecdfbde57a6177aa9f7491056
pebblerye <bertvandenbosch76@gmail.com>
2025-10-07 10:26:32 +0200

    Automated commit di  7 okt 2025 10:26:32 CEST

--- rosen-watcher-pwa/src/app/service/http.download.service.ts
+++ rosen-watcher-pwa/src/app/service/http.download.service.ts
@@ -4,12 +4,14 @@ import { HttpClient } from '@angular/common/http';
 import { catchError, map } from 'rxjs/operators';
 import { firstValueFrom } from 'rxjs';
 import { EventService, EventType } from './event.service';
+import { WatcherInfo } from '../../service/ts/models/watcher.info';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HttpDownloadService {
   private activeDownloads: Record<string, boolean> = {};
+  private useNode = true;
   constructor(
     private http: HttpClient,
     private eventService: EventService,
@@ -43,11 +45,22 @@ export class HttpDownloadService {
     }
   }
 
+  downloadBalance(address: string): Observable<WatcherInfo> {
+    if (!this.useNode) {
+      const url = `https://${rs_ErgoExplorerHost}/api/v1/addresses/${address}/balance/confirmed`;
+      return this.downloadStream<WatcherInfo>(url);
+    } else {
+      const url = `https://${rs_ErgoNodeHost}/blockchain/balance`;
+      let result = this.downloadPostStream<{ confirmed: WatcherInfo }>(url, address);
+      return result.pipe(map((data: { confirmed: WatcherInfo }) => data.confirmed as WatcherInfo));
+    }
+  }
+
   downloadStream<T>(url: string): Observable<T> {
     this.initiateDownload();
     console.log('Attempting to load from cache:', url);
     this.activeDownloads[url] = true;
-    // Check if the data exists in the cache
+
     const cachedData = localStorage.getItem(url);
     let cacheObservable: Observable<T>;
 
@@ -56,7 +69,7 @@ export class HttpDownloadService {
       cacheObservable = of(JSON.parse(cachedData));
     } else {
       console.log('No cache available:', url);
-      cacheObservable = EMPTY; // Observable that completes immediately
+      cacheObservable = EMPTY;
     }
 
     const downloadObservable = this.http.get<T>(url).pipe(
@@ -75,7 +88,42 @@ export class HttpDownloadService {
       }),
     );
 
-    // First emit cached data if available, then try to download and emit the new data
+    return concat(cacheObservable, downloadObservable);
+  }
+
+  downloadPostStream<T>(url: string, body: string): Observable<T> {
+    let downloadKey = url + body;
+    this.initiateDownload();
+    console.log('Attempting to load from cache:', downloadKey);
+    this.activeDownloads[downloadKey] = true;
+    // Check if the data exists in the cache
+    const cachedData = localStorage.getItem(downloadKey);
+    let cacheObservable: Observable<T>;
+
+    if (cachedData) {
+      console.log('Loaded from cache:', downloadKey);
+      cacheObservable = of(JSON.parse(cachedData));
+    } else {
+      console.log('No cache available:', downloadKey);
+      cacheObservable = EMPTY;
+    }
+
+    const downloadObservable = this.http.post<T>(url, body).pipe(
+      map((results: T) => {
+        console.log('Downloaded from server:', url);
+        localStorage.setItem(downloadKey, JSON.stringify(results));
+
+        this.endDownload(downloadKey);
+        return results;
+      }),
+      catchError((error) => {
+        console.log('Download failed:', url);
+
+        this.endDownload(downloadKey);
+        return throwError(error);
+      }),
+    );
+
     return concat(cacheObservable, downloadObservable);
   }
 }
--- rosen-watcher-pwa/src/app/service/watchers.data.service.ts
+++ rosen-watcher-pwa/src/app/service/watchers.data.service.ts
@@ -16,10 +16,6 @@ export class WatchersDataService {
   isChainTypeActive(chainType: ChainType): boolean {
     return rewardAddresses[chainType] !== null;
   }
-  readonly watcherUrl =
-    'https://' +
-    rs_ErgoExplorerHost +
-    '/api/v1/addresses/ChTbcUHgBNqNMVjzV1dvCb2UDrX9nh6rGGcURCFEYXuH5ykKh7Ea3FvpFhHb9AnxXJkgAZ6WASN7Rdn7VMgkFaqP5Z5RWp84cDTmsZkhYrgAVGN7mjeLs8UxqUvRi2ArZbm35Xqk8Y88Uq2MJzmDVHLHzCYRGym8XPxFM4YEVxqzHSKYYDvaMLgKvoskFXKrvceAqEiyih26hjpekCmefiF1VmrPwwShrYYxgHLFCZdigw5JWKV4DmewuR1FH3oNtGoFok859SXeuRbpQfrTjHhGVfDsbXEo3GYP2imAh1APKyLEsG9LcE5WZnJV8eseQnYA8sACLDKZ8Tbpp9KUE7QZNFpnwGnkYx7eybbrCeFDFjTGpsBzaS6fRKrWj2J4Wy3TTyTU1F8iMCrHBF8inZPw9Kg9YEZuJMdXDFNtuaK15u86mF2s2Z5B1vdL5MtZfWThFLnixKds8ABEmGbe8n75Dym5Wv3pkEXQ6XPpaMjUxHfRJB3EfcoFM5nsZHWSTfbFBcHxSRnEiiU67cgJsBUpQn7FvEvqNLiKM4fL3yyykMtQ6RjAS8rhycszphvQa5qFrDHie4vPuTq8/balance/confirmed';
 
   readonly rsnToken = '8b08cdd5449a9592a9e79711d7d79249d7a03c535d17efaee83e216e80a44c4b';
   readonly watchersStatsSignal = signal<WatchersStats>(new WatchersStats());
@@ -108,7 +104,9 @@ export class WatchersDataService {
   }
 
   getWatchersInfo(): Observable<WatcherInfo> {
-    const result = this.downloadService.downloadStream<WatcherInfo>(this.watcherUrl);
+    const result = this.downloadService.downloadBalance(
+      'ChTbcUHgBNqNMVjzV1dvCb2UDrX9nh6rGGcURCFEYXuH5ykKh7Ea3FvpFhHb9AnxXJkgAZ6WASN7Rdn7VMgkFaqP5Z5RWp84cDTmsZkhYrgAVGN7mjeLs8UxqUvRi2ArZbm35Xqk8Y88Uq2MJzmDVHLHzCYRGym8XPxFM4YEVxqzHSKYYDvaMLgKvoskFXKrvceAqEiyih26hjpekCmefiF1VmrPwwShrYYxgHLFCZdigw5JWKV4DmewuR1FH3oNtGoFok859SXeuRbpQfrTjHhGVfDsbXEo3GYP2imAh1APKyLEsG9LcE5WZnJV8eseQnYA8sACLDKZ8Tbpp9KUE7QZNFpnwGnkYx7eybbrCeFDFjTGpsBzaS6fRKrWj2J4Wy3TTyTU1F8iMCrHBF8inZPw9Kg9YEZuJMdXDFNtuaK15u86mF2s2Z5B1vdL5MtZfWThFLnixKds8ABEmGbe8n75Dym5Wv3pkEXQ6XPpaMjUxHfRJB3EfcoFM5nsZHWSTfbFBcHxSRnEiiU67cgJsBUpQn7FvEvqNLiKM4fL3yyykMtQ6RjAS8rhycszphvQa5qFrDHie4vPuTq8',
+    );
 
     return result;
   }
@@ -237,10 +235,8 @@ export class WatchersDataService {
   }
 
   private downloadPermitInfo(address: string, tokenId: string | null, tokenName: string | null) {
-    const permitsUrl = `https://${rs_ErgoExplorerHost}/api/v1/addresses/${address}/balance/confirmed`;
-
     return this.downloadService
-      .downloadStream<WatcherInfo>(permitsUrl)
+      .downloadBalance(address)
       .pipe(
         map((data: { tokens: Token[] }) => {
           if (data.tokens) {
@@ -248,7 +244,7 @@ export class WatchersDataService {
               (token: Token) =>
                 (tokenId && token.tokenId === tokenId) || (tokenName && token.name === tokenName),
             );
-            console.log(permitsUrl);
+
             if (tokenData) {
               tokenData.amount /= rs_PermitCost * Math.pow(10, tokenData.decimals);
               tokenData.amount = Math.floor(tokenData.amount);
