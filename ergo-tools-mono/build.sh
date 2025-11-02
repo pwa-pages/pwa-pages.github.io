f7ee8bcae75c75028b792093abf63b5e7c4636d5
pebblerye <bertvandenbosch76@gmail.com>
2025-11-02 19:02:50 +0100

    Automated commit zo  2 nov 2025 19:02:49 CET

--- ergo-tools-mono/build.sh
+++ ergo-tools-mono/build.sh
@@ -1,9 +1,19 @@
 ./clean.sh
 npm install
 nx build service
-#nx build rosen-watcher-pwa --configuration production
+nx build rosen-watcher-pwa --configuration production
+nx build rosen-watcher-elements --configuration production
+
+./prep_web_component.sh
+
+
+mv web-component/* ../ergo-tools-mono-app/web-component/
+./clean.sh
+cp -R dist/apps/rosen-watcher-pwa/browser/* ../ergo-tools-mono-app
+
+npm install
+nx build service
 nx build rosen-watcher-pwa --configuration development
 nx build rosen-watcher-elements --configuration development
-#ng build rosen-web-component --configuration production
-#npm run prep_web_component
-#cp dist/rosen-watcher-pwa/browser/ngsw-worker.js ./src/shared/js/
+
+
