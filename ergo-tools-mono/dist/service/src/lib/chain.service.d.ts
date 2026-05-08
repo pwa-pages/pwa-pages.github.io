a540aefaf4caa809826a4345e80f6f0c50edd0d3
pebblerye <bertvandenbosch76@gmail.com>
2026-05-08 12:44:48 +0200

    Automated commit vr  8 mei 2026 12:44:48 CEST

--- ergo-tools-mono/dist/service/src/lib/chain.service.d.ts
+++ ergo-tools-mono/dist/service/src/lib/chain.service.d.ts
@@ -25,6 +25,7 @@ declare const permitAddresses: Record<ChainType, string | null>;
 declare const permitTriggerAddresses: Record<ChainType, string | null>;
 declare const permitBulkAddresses: Record<ChainType, string | null>;
 declare const rewardAddresses: Record<ChainType, string | null>;
+declare const rewardAddressesV2: Record<ChainType, string | null>;
 declare const hotWalletAddress = "nB3L2PD3J4rMmyGk7nnNdESpPXxhPRQ4t1chF8LTXtceMQjKCEgL2pFjPY6cehGjyEFZyHEomBTFXZyqfonvxDozrTtK5JzatD8SdmcPeJNWPvdRb5UxEMXE4WQtpAFzt2veT8Z6bmoWN";
 /**
  * Determines the ChainType based on the provided address.
