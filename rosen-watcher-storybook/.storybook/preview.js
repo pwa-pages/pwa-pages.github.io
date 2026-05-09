6191685bf1461293e488f36218aca4aaac4d7b40
pebblerye <bertvandenbosch76@gmail.com>
2026-05-09 14:32:18 +0200

    Automated commit za  9 mei 2026 14:32:18 CEST

--- rosen-watcher-storybook/.storybook/preview.js
+++ rosen-watcher-storybook/.storybook/preview.js
@@ -2,14 +2,14 @@
 document.body.insertAdjacentHTML('beforeend', '<rosen-watcher-component></rosen-watcher-component>');
 
 const script = document.createElement('script');
-script.src = './rosen-watcher-components.3.85.1.js'; 
+script.src = './rosen-watcher-components.3.86.5.js'; 
 script.type = 'module';    
 script.async = false;
 document.body.appendChild(script);
 
 const link = document.createElement('link');
 link.rel = 'stylesheet';
-link.href = './styles-3YGY4CDT.css'; 
+link.href = './styles-YQ6JHXVS.css'; 
 document.head.appendChild(link);
 
 export const parameters = {
