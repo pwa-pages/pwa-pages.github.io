649aac7c1396899df2540212fcb42d4f817f156a
pebblerye <bertvandenbosch76@gmail.com>
2026-09-12 12:41:33 +0200

    Automated commit za 12 sep 2026 12:41:33 CEST

--- watcher_scripts/set_watcher_generic.sh
+++ watcher_scripts/set_watcher_generic.sh
@@ -6,7 +6,7 @@ source /home/pebblerye/crypto_scripts/set_heights.sh
 echo "Extract mnemonic from local.yaml"
 mnemonic=$(sed -n 's/.*mnemonic: "\([^"]*\)".*/\1/p' "local.yaml.prev")
 echo "Extract api key from local.yaml"
-apikey=$(sed -n 's/.*apiKeyHash: "\([^"]*\)".*/\1/p' "local.yaml.prev")
+apikey=$(sed -n "s/.*apiKeyHash: '\([^']*\)'.*/\1/p" "local.yaml.prev")
 
 echo "Setting ergo height to local.yaml: $ergo_height"
 
